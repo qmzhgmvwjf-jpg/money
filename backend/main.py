c0a1c4bda1cd688f969974dfbb60300d319ce399
김재훈 <jaehoon@gimjaehun-ui-MacBookPro.local>
2026-04-19 14:44:06 +0900

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -31,7 +31,7 @@ app.add_middleware(
         "https://money-sepia-beta.vercel.app"
     ],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
