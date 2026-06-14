97f429fa36b1deae7c0048ebbd61aec1dbbec945
김재훈 <jaehoon@gimjaehun-ui-MacBookPro.local>
2026-06-14 23:13:56 +0900

    change app name & favicon

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
-from routers import admin, auth, driver, menus, notices, orders, store
+from routers import admin, auth, driver, menus, orders, store
 from services.platform_service import ensure_default_admin
 
 app = FastAPI()
